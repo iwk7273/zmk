169ed5afab8fbded550398c1183d3991203f3739
iwk7273 <iwk.nov16@gmail.com>
2026-06-08 23:29:46 +0900

    Revert "fix(studio): make BLE RPC TX buffer write timeout configurable"
    This reverts commit 0dce2b1097aa96cbed6506e12db0a6a859a9344a.

--- app/src/studio/rpc.c
+++ app/src/studio/rpc.c
@@ -126,7 +126,7 @@ RING_BUF_DECLARE(rpc_tx_buf, CONFIG_ZMK_STUDIO_RPC_TX_BUF_SIZE);
 
 struct ring_buf *zmk_rpc_get_tx_buf(void) { return &rpc_tx_buf; }
 
-#define RPC_TX_BUFFER_WAIT_TIMEOUT_MS CONFIG_ZMK_STUDIO_RPC_TX_BUFFER_WAIT_TIMEOUT_MS
+#define RPC_TX_BUFFER_WAIT_TIMEOUT_MS 20
 
 struct rpc_tx_stream_state {
     void *user_data;
